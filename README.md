7f135258997a6ee0b6323c07ba4792667deacb5c
Luís Felipe Henriques <luis.henriques@estudante.ufscar.br>
2024-07-09 08:01:48 -0300

    config: começo do projeto com o banco

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NLWJourneyPython
+
+Criação de um serviço para organização de viagens, sejam elas de trabalho ou lazer.
+
 Repositório utilizado para aprender python através do bootcamp da NLW trilha de python
---
+++ init/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE IF NOT EXISTS 'trips' (
+    id TEXT PRIMARY KEY
+    destination TEXT NOT NULL,
+    start_date DATETIME,
+    end_date DATETIME,
+    owner_name TEXT NOT NULL,
+    owner_email TEXT NOT NULL,
+    status INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS emails_to_invite (
+    id TEXT PRIMARY KEY,
+    trip_id TEXT REFERENCES trips(id),
+    email TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS 'links' (
+    id TEXT PRIMARY KEY
+    trip_id TEXT,
+    link TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES trips(id)
+);
