72e1d24e966baee495f00904c11277b9be0834ca
Luís Felipe Henriques <luis.henriques@estudante.ufscar.br>
2024-07-17 07:41:41 -0300

    fix: tests github actions

---
+++ init/init_db.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+conn = psycopg2.connect(
+    database='postgres',
+    user="postgres",
+    password="root",
+    host="localhost",
+    port="5432",
+)
+
+conn.autocommit = True
+
+cursor = conn.cursor()
+
+CREATE_SCHEMA = 'CREATE SCHEMA IF NOT EXISTS "NLWJourney2";'
+
+# Open and read the file as a single buffer
+fd = open('schema.sql', 'r')
+sqlFile = fd.read()
+fd.close()
+
+sqlCommands = sqlFile.split(';')
+
+cursor.execute(CREATE_SCHEMA)
+
+# Execute every command from the input file
+for command in sqlCommands:
+    try:
+        cursor.execute(command)
+    except Exception as e:
+        print("Command skipped: ", str(e))
+
+conn.close()
--- init/schema.sql
+++ init/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS "NLWJourney".trips (
+CREATE TABLE IF NOT EXISTS "NLWJourney2".trips (
     id TEXT PRIMARY KEY,
     destination TEXT NOT NULL,
     start_date TIMESTAMP,
@@ -8,33 +8,35 @@ CREATE TABLE IF NOT EXISTS "NLWJourney".trips (
     status INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS "NLWJourney".emails_to_invite (
+CREATE TABLE IF NOT EXISTS "NLWJourney2".emails_to_invite (
     id TEXT PRIMARY KEY,
-    trip_id TEXT REFERENCES trips(id),
-    email TEXT NOT NULL
+    trip_id TEXT,
+    email TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES "NLWJourney2".trips(id)
 );
 
-CREATE TABLE IF NOT EXISTS "NLWJourney".links (
+CREATE TABLE IF NOT EXISTS "NLWJourney2".links (
     id TEXT PRIMARY KEY,
-    trip_id TEXT REFERENCES trips(id),
+    trip_id TEXT,
     link TEXT NOT NULL,
-    title TEXT NOT NULL
+    title TEXT NOT NULL,
+    FOREIGN KEY (trip_id) REFERENCES "NLWJourney2".trips(id)
 );
 
-CREATE TABLE IF NOT EXISTS "NLWJourney".participants (
+CREATE TABLE IF NOT EXISTS "NLWJourney2".participants (
     id TEXT PRIMARY KEY,
-    trip_id TEXT NOT NULL,
-    emails_to_invite_id TEXT NOT NULL,
+    trip_id TEXT,
+    emails_to_invite_id TEXT,
     name TEXT NOT NULL,
     is_confirmed INTEGER,
-    FOREIGN KEY (trip_id) REFERENCES "NLWJourney".trips(id),
-    FOREIGN KEY (emails_to_invite_id) REFERENCES "NLWJourney".emails_to_invite(id)
+    FOREIGN KEY (trip_id) REFERENCES "NLWJourney2".trips(id),
+    FOREIGN KEY (emails_to_invite_id) REFERENCES "NLWJourney2".emails_to_invite(id)
 );
 
-CREATE TABLE IF NOT EXISTS "NLWJourney".activities (
+CREATE TABLE IF NOT EXISTS "NLWJourney2".activities (
     id TEXT PRIMARY KEY,
     trip_id TEXT,
     title TEXT NOT NULL,
     occurs_at TIMESTAMP,
-    FOREIGN KEY (trip_id) REFERENCES "NLWJourney".trips(id)
-);
+    FOREIGN KEY (trip_id) REFERENCES "NLWJourney2".trips(id)
+)
