15ddd1abedcd69045f52653bc5856f98eb21e3b9
Luís Felipe Henriques <luis.henriques@estudante.ufscar.br>
2024-07-18 12:35:12 -0300

    fix: data

--- src/main/routes/tests/conftest.py
+++ src/main/routes/tests/conftest.py
@@ -8,8 +8,8 @@ import uuid
 def trip_to_create_info():
     return {
             "destination":"Joinville",
-            "start_date":"12-07-2024",
-            "end_date":"17-07-2024",
+            "start_date":"12/07/2024",
+            "end_date":"17/07/2024",
             "owner_name":"Fernando",
             "owner_email": "trips@email.com",
             "emails_to_invite":[]
